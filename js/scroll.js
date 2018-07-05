41e39bc3921169ebe0156679f56000cd0adecca6
Lea Lai Van <l.laivan@uqconnect.edu.au>
2018-07-05 12:24:30 -0800

    Updated: Continuous Responsiveness of the landing container

--- js/scroll.js
+++ js/scroll.js
@@ -1,24 +1,14 @@
 $(document).ready(function(){
     console.log("Scroll is ready!");
     
-    //Set the background image to the landing page
-    var img = $('.cover-container').css('background-image', 'url("css/images/bg.jpg")');
-
-    //Retrieve the screen height of the user
-    var screenheight = screen.height;
-    
-    //console.log(screenheight);
-    
-    //Adapt the landing container to have the screenheight minus 100px
-    $('.landing').css('height',screenheight-100);
-    
     $('a').on('click', function(event) {
         var target = $(this.getAttribute('href'));
         console.log(target);
         if( target.length ) {
             event.preventDefault();
             $('html, body').animate({
-                scrollTop: target.offset().top-79
+                //scrollTop: target.offset().top-79
+                scrollTop: target.offset().top-20
             }, 1000);
         }
     });
