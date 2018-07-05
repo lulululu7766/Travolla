52762e0141ba77d890a934e9d8f7d7c3af3b8236
Henry Burgess <henryjamesburgess@bigpond.com>
2018-07-05 10:33:13 +0800

    Updated OJP
    Updated:
    - Enables and disables buttons when locations added / removed.

--- js/journeyplanner.js
+++ js/journeyplanner.js
@@ -1,16 +1,22 @@
-// Create a "close" button and append it to each list item
-var activityList = document.getElementsByTagName("li");
-for (i = 0; i < activityList.length; i++) {
-  	var span = document.createElement("span");
-	var txt = document.createTextNode("\u00D7");
-	span.className = "close";
-	span.appendChild(txt);
-	activityList[i].appendChild(span);
+function addButtons() {
+	var locations = ["DNUI", "Dalian Modern Museum", "Salmon Restaurant", "Wanda Plaza"];
+	var btnDiv = document.getElementById('activityButtons');
+	var getBtn = document.getElementById('getBtn');
+	for (x = 0; x < locations.length; x++) {
+		var btn = document.createElement("button");
+		btn.className = "btn btn-primary";
+		btn.innerText = locations[x];
+		btn.onclick = function() {
+			addActivity(this.innerText);
+		};
+		btnDiv.appendChild(btn);
+	}
+	getBtn.disabled = true;
 }
 
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
-for (i = 0; i < close.length; i++) {
+for (z = 0; z < close.length; z++) {
 	close[i].onclick = function() {
 		var div = this.parentElement;
 		div.style.display = "none";
@@ -19,9 +25,12 @@ for (i = 0; i < close.length; i++) {
 
 // Function to add a new activity.
 function addActivity(text) {
+	var buttonList = document.getElementsByClassName("btn btn-primary");
 	var li = document.createElement("li");
 	var t = document.createTextNode(text);
-	li.appendChild(t);
+	var txtDiv = document.createElement("div");
+	txtDiv.appendChild(t);
+	li.appendChild(txtDiv);
 	document.getElementById("activityList").appendChild(li);
 	
 	var span = document.createElement("span");
@@ -33,10 +42,29 @@ function addActivity(text) {
 	li.appendChild(span);
 	li.className = "list-group-item";
 	
-	for (i = 0; i < close.length; i++) {
-		close[i].onclick = function() {
+	for (b = 0; b < buttonList.length; b++) {
+		if (buttonList[b].innerText === text) {
+			buttonList[b].disabled = true;
+		}
+	}
+	
+	for (a = 0; a < close.length; a++) {
+		close[a].onclick = function() {
 			var div = this.parentElement;
 			div.style.display = "none";
+			var elementList = this.parentElement.children;
+			enableButton(elementList[0].innerText);
+			//console.log();
+			
+		}
+	}
+}
+
+function enableButton(btnName) {
+	var buttonList = document.getElementsByClassName("btn btn-primary");
+	for (b = 0; b < buttonList.length; b++) {
+		if (buttonList[b].innerText === btnName) {
+			buttonList[b].disabled = false;
 		}
 	}
 }
