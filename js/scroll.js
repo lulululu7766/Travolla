9c5c1628137c74d7479e894dd480efcb2674d7e0
Lea Lai Van <l.laivan@uqconnect.edu.au>
2018-07-05 11:44:37 -0800

    Added: responsiveness of the landing container in scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
     console.log("Scroll is ready!");
-  
+    
+    //Set the background image to the landing page
+    var img = $('.cover-container').css('background-image', 'url("css/images/bg.jpg")');
+
+    //Retrieve the screen height of the user
+    var screenheight = screen.height;
+    
+    //console.log(screenheight);
+    
+    //Adapt the landing container to have the screenheight minus 100px
+    $('.landing').css('height',screenheight-100);
+    
     $('a').on('click', function(event) {
         var target = $(this.getAttribute('href'));
         console.log(target);
@@ -11,5 +22,7 @@ $(document).ready(function(){
             }, 1000);
         }
     });
+    
+   
 });
 
