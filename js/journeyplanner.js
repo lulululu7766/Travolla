626d6ee8b20e8d647b8377991ab522d21dc914ba
Henry Burgess <henryjamesburgess@bigpond.com>
2018-07-06 01:02:01 +0800

    Updated OJP.
    Added reset button to main page of OJP, to reset the journey. Also added Bootstrap radio buttons to select the pace at which people want their day to be. Rewrote all Journey Planner JS, less disgusting, actually has some comments! Todo: Actual optimiser is very broken.

--- js/journeyplanner.js
+++ js/journeyplanner.js
@@ -2,7 +2,7 @@ window.onload = function() {
 	getActivites()
 }
 
-var activityDict = {"DNUI":2, "Dalian Modern Museum":3, "Salmon Restaurant":2, "Wanda Plaza":4};
+var activityDict = {"DNUI":1, "Dalian Modern Museum":1, "Salmon Restaurant":2, "Wanda Plaza":3};
 
 var currentActivities = [];
 
@@ -48,6 +48,7 @@ function removeActivity(activityName) {
 	drawActivities();
 }
 
+// Draw activities as Bootstrap cards
 function drawActivities() {
 	// Create Bootstrap for card
 	var cardsDiv = document.getElementById('cardsDiv');
@@ -57,7 +58,6 @@ function drawActivities() {
 	for (i = 0; i < currentActivities.length; i++) {
 		var activityName = currentActivities[i];
 		var activityDuration = activityDict[activityName];
-		console.log("Name: " + activityName + ", Duration: " + activityDuration);
 		// Create new card
 		var card = document.createElement('div');
 		card.className = "card md-3";
@@ -96,8 +96,6 @@ function drawActivities() {
 		card.appendChild(cardBody);
 		cardsDiv.append(card);
 	}
-	
-	
 }
 
 // Activate activity button
@@ -120,195 +118,74 @@ function deactivateButton(activityName) {
 	}
 }
 
+// The big boy.
+function optimiseJourney() {
+	var timetableTodo = currentActivities;
+	var maxLength;
+	// Initialise totalTime as 1 hour for each activity
+	var totalTime = currentActivities.length;
+	for (i = 0; i < currentActivities.length; i++) {
+		totalTime += activityDict[currentActivities[i]];
+	}
+	// Get days
+	var totalDays = document.getElementById('daysInput').value;
+	// Get day length
+	// Check if there is not enough time in the day
+	if ((document.getElementById('shortDay')).checked) {
+		maxLength = 4;	
+	} else if ((document.getElementById('normalDay')).checked) {
+		maxLength = 6;
+	} else {
+		maxLength = 8;
+	}
+	for (i = 0; i < currentActivities.length; i++) {
+		if (activityDict[currentActivities[i]] > maxLength - 1) {
+			console.log(totalTime);
+			alert("One activity is too long.");
+		}
+	}
+	
+	var timetableDict = {};
+	console.log(totalDays);
+	var filledTime = 0;
+	var day = 1;
+	var activitiesPerDay = [];
+	for (i = 0; i < timetableTodo.length; i++) {
+		var activityName = timetableTodo[i];
+		var activityDuration = 1 + activityDict[activityName];
+		if (filledTime + activityDuration <= maxLength) {
+			activitiesPerDay.push("Transit");
+			activitiesPerDay.push(activityName);
+			filledTime += activityDuration;
+			timetableDict[i] = activitiesPerDay;
+		} else {
+			day++;
+			activitiesPerDay = [];
+			filledTime = 0;
+		}
+	}
+		
+	console.log(timetableDict);
+	
+}
+
+// Reset journey
+function resetJourney() {
+	currentActivities = [];
+	document.getElementById('daysInput').value = 1;
+	resetButtons();
+	drawActivities();
+}
+
+// Enable all buttons
+function resetButtons() {
+	var buttonList = document.getElementsByClassName('activityBtn btn btn-secondary');
+	for (i = 0; i < buttonList.length; i++) {
+		buttonList[i].disabled = false;
+	}
+}
+
+// General function to trim trailing whitespace from strings.
 function trim(s) { 
   return ( s || '' ).replace( /^\s+|\s+$/g, '' ); 
 }
-
-/**
-// Add activity buttons
-function addButtons() {
-	var locationDiv = document.getElementById('locationDiv');
-	var locationText = document.createElement('h4');
-	locationText.innerText = "Events near *location*";
-	locationDiv.appendChild(locationText);
-	var locations = ["DNUI", "Dalian Modern Museum", "Salmon Restaurant", "Wanda Plaza"];
-	var btnDiv = document.getElementById('activityDiv');
-	for (x = 0; x < locations.length; x++) {
-		var btn = document.createElement("button");
-		btn.className = "btn btn-primary";
-		btn.innerText = locations[x];
-		btn.onclick = function() {
-			addActivity(this.innerText);
-		};
-		btnDiv.appendChild(btn);
-	}
-}
-**/
-
-/**
-// Function to add a new activity.
-function addActivity(text) {
-	var buttonList = document.getElementsByClassName("btn btn-primary");
-	var cardDiv = document.createElement("div");
-	var activityDiv = document.getElementById("cardsDiv");
-	cardDiv.className = "card bg-light mb-4";
-	cardDiv.setAttribute("style","max-width:100%;");
-
-	var contentDiv = document.createElement("div");
-	contentDiv.className = "card-header";
-	
-	var t = document.createElement("h5");
-	t.innerText = text;
-	
-	var contentText = document.createElement("div");
-	contentText.className = "card-body text-dark";
-	var description = document.createElement("p");
-	description.innerText = "Lorem Ipsum Dipsum";
-	contentText.appendChild(description);
-	
-	contentDiv.appendChild(t);
-	cardDiv.appendChild(contentDiv);
-	cardDiv.appendChild(contentText);
-	activityDiv.appendChild(cardDiv);
-	
-	var closeDiv = document.createElement("div");
-	closeDiv.className = "closable";
-	closeDiv.setAttribute("style", "padding: 10px; width: 100%; text-align: right;")
-	
-	var removeButton = document.createElement("button");
-	removeButton.className = "btn btn-danger";
-	removeButton.innerText = "Remove";
-	
-	closeDiv.appendChild(removeButton);
-	
-	cardDiv.appendChild(closeDiv);
-	
-	var activityCost = document.createElement("div");
-	activityCost.className = "card-footer";
-	var timeCost = document.createElement("b");
-	timeCost.innerText = 3;
-	activityCost.appendChild(timeCost);
-	cardDiv.appendChild(activityCost);
-
-	for (b = 0; b < buttonList.length; b++) {
-		if (buttonList[b].innerText === text) {
-			buttonList[b].disabled = true;
-		}
-	}
-	
-	for (a = 0; a < close.length; a++) {
-		close[a].onclick = function() {
-			var div = this.parentElement;
-			div.style.display = "none";
-			var elementList = this.parentElement.children;
-			enableButton(elementList[0].innerText);			
-		}
-	}
-	updateTime();
-}
-
-function enableButton(btnName) {
-	var buttonList = document.getElementsByClassName("btn btn-primary");
-	for (b = 0; b < buttonList.length; b++) {
-		if (buttonList[b].innerText === btnName) {
-			buttonList[b].disabled = false;
-		}
-	}
-}
-
-function updateTime() {
-	var totalTime = 0;
-	var timeDiv = document.getElementById("timeDiv");
-	timeDiv.innerHTML = "<h5>Total Time:</h5>";
-	var timeText = document.createElement("p");
-	var activityCards = document.getElementsByClassName("card bg-light mb-4");
-	for (c = 0; c < activityCards.length; c++) {	
-		totalTime += parseInt((activityCards[c].children)[3].innerText);
-	}
-	timeText.append(totalTime);
-	timeDiv.append(timeText)
-	console.log(totalTime);
-}
-
-function getDays() {
-	var days = parseInt(document.getElementById('daysInput').value);
-	console.log(days);
-	return days;
-}
-
-function makeActivitiesDict() {
-	var dict = {};
-	var activityCards = document.getElementsByClassName("card bg-light mb-4");
-	for (c = 0; c < activityCards.length; c++) {
-		dict[trim((activityCards[c].children)[0].innerText)] = parseInt((activityCards[c].children)[3].innerText);
-		dict["Travel Time " + c] = 1;
-	}
-	console.log(dict);
-	return dict;
-}
-
-function getActivities(dict) {
-	var activityList = [];
-	for (var activity in dict) {
-		activityList.push(activity);
-	}
-	console.log(activityList);
-	return activityList;
-}
-
-function optimise() {
-	// Check if too many activities for number of days
-	var activitiesDict = makeActivitiesDict();
-	var numDays = getDays();
-	var totalTime = 0;
-	var avgDay = 0;
-	
-	for (var actName in activitiesDict) {
-		totalTime += activitiesDict[actName];
-	}
-	
-	console.log(totalTime);
-	
-	avgDay = totalTime / numDays;
-	
-	if (avgDay > 8) {
-		console.log("Days are too long.")
-		alert("Days are too long!");
-	}
-	
-	drawTimetable(activitiesDict);
-}
-
-function drawTimetable(fullDict) {
-	var timetableDiv = document.getElementById('timetableParent');
-	timetableDiv.innerHTML = '<h2>Your Timetable</h2><ul id="timetable" class="list-group"></ul>';
-	
-	var activityDict = fullDict;
-	var activityList = getActivities(activityDict);
-	var dict = fullDict;
-	var timetableList = document.getElementById('timetable');
-	var dictLength = 0;
-	for (var i in dict) {
-		if (dict.hasOwnProperty(i)) {
-			dictLength++;
-		}
-	}
-	console.log(dictLength);
-	for (i = 0; i < dictLength; i++) {
-		var timetableEvent = document.createElement("li");
-		timetableEvent.className = "list-group-item";
-		var activityName = document.createElement("h5");
-		var activityTime = document.createElement("p");
-		activityName.innerText = activityList[i];
-		var activityTxt = activityList[i];
-		activityTime.innerText = dict[activityTxt];
-		
-		timetableEvent.append(activityName);
-		timetableEvent.append(activityTime);
-		timetableList.append(timetableEvent);
-	}
-	
-}
-
-
-**/
