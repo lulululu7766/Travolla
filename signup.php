ab2cecc1eb452c4f5ac13a31da76291c1650201b
Lea Lai Van <l.laivan@uqconnect.edu.au>
2018-07-06 14:42:29 -0800

    Added: login feature checking from the database, decryption, alerts to inform the user about wrong inputs

--- signup.php
+++ signup.php
@@ -78,14 +78,14 @@
 <body  onload="displayWindowSize()" onresize="displayWindowSize()">	  
     
     <nav class="navbar navbar-expand-md fixed-top bg-dark">
-      <a class="navbar-brand" href="index.html"><img src="css/images/teamlogo.png"></a>
+      <a class="navbar-brand" href="index.php"><img src="css/images/teamlogo.png"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="index.html">Home </a>
+            <a class="nav-link" href="index.php">Home </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="journeyplanner.html"> Journey Planner</a>
