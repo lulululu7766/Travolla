aafd423cf9401216a464b5a0bb57dc0d380e72e2
Henry Burgess <henryjamesburgess@bigpond.com>
2018-07-09 20:06:24 +0800

    Added container heatmap.php
    A container that embeds heatmap.html.

---
+++ heatmap.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="./css/bootstrap.css">
+        <link rel="shortcut icon" href="css/images/weblogo.png"/>
+        <link href="https://fonts.googleapis.com/css?family=Patua+One" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+        <title>Travello - HeatMap</title>
+    </head>
+    <body>
+        <div class="container-fluid" style="height: 90vh; width: 90vw">
+            <h1>HeatMap</h1>
+            <object data="heatmap.html" width="100%" style="min-width: 100%; min-height: 100%;"> </object>
+        </div>
+    </body>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="./js/bootstrap.bundle.js"></script>
+</html>
